0a84293ce04efd739d6a29e4d41dec95a2a1130f
Rhitam <rhitamnayek17@gmail.com>
2025-03-05 14:01:50 +0530

    njnjbnvffbrf

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Load model & vectorizer
 model = pickle.load(open("model.pkl", "rb"))
